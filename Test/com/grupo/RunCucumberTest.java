2a8fa3878edfda8902e0f970d54c0d49c200819e
ivonnelizethmoreno-cloud <ivonnelizethmoreno@gmail.com>
2025-10-29 01:12:07 -0500

    Update RunCucumberTest.java

--- Test/com/grupo/RunCucumberTest.java
+++ Test/com/grupo/RunCucumberTest.java
@@ -1,18 +1,17 @@
 package com.grupo;
 
 import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.ConfigurationParameter;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
+@SelectClasspathResource("features")  // 👈 Debe coincidir con target/test-classes/features
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.grupo")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json")
 public class RunCucumberTest { }
+
