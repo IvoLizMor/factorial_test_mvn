cc03bb075ce3275ee1aab55d1f9826b939324b06
ivonnelizethmoreno-cloud <ivonnelizethmoreno@gmail.com>
2025-10-29 00:31:46 -0500

    Update RunCucumberTest.java

--- Test/com/grupo/RunCucumberTest.java
+++ Test/com/grupo/RunCucumberTest.java
@@ -1,8 +1,18 @@
 package com.grupo;
- 
-import io.cucumber.junit.platform.engine.Cucumber;
- 
-@Cucumber
-public class RunCucumberTest {
-    // No necesita código adicional
-}
+
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.ConfigurationParameter;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.grupo")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:features")
+public class RunCucumberTest { }
