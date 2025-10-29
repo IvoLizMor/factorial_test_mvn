4dd1c71ef06af0da7a4e3403ce140811c6ff923b
ivonnelizethmoreno-cloud <ivonnelizethmoreno@gmail.com>
2025-10-29 01:22:51 -0500

    Change GLUE_PROPERTY_NAME to an empty string
    Updated GLUE_PROPERTY_NAME configuration parameter to an empty value.

--- Test/com/grupo/RunCucumberTest.java
+++ Test/com/grupo/RunCucumberTest.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")  // 👈 Debe coincidir con target/test-classes/features
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.grupo")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json")
 public class RunCucumberTest { }
 
