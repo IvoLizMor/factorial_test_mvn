b720ee62b5c2d842cc12474be46efa1f57c10a7c
ivonnelizethmoreno-cloud <ivonnelizethmoreno@gmail.com>
2025-10-30 20:51:19 -0500

    Update RunCucumberTest.java

--- Test/com/grupo/RunCucumberTest.java
+++ Test/com/grupo/RunCucumberTest.java
@@ -1,16 +1,19 @@
 package com.grupo;
 
-import org.junit.platform.suite.api.*;
-import static io.cucumber.junit.platform.engine.Constants.*;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.ConfigurationParameter;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
+// Los .feature se cargan desde el classpath de test, en la carpeta "features/"
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.grupo")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.grupo") // paquete donde pondrás steps (o vacío por ahora)
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber-report.json")
 public class RunCucumberTest { }
-
-
-
-
-
