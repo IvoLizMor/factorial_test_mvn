fc9a6afbf203e229906c2e16349888804b381a72
robertbarreto7 <robertbarreto7@outlook.com>
2025-11-04 23:29:24 -0500

    Create ErrorMessages.java

---
+++ src/main/java/com/grupo/errors/ErrorMessages.java
@@ -0,0 +1,7 @@
+package com.grupo.errors;
+
+public class ErrorMessages {
+    public static final String NEG = "No se permiten números negativos";
+    public static final String VAC = "Debe ingresar un valor";
+    public static final String INV = "Ingrese solo números enteros";
+}
