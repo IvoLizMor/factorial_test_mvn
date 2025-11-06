1ce4cd9eb224328d46309728de84d79d400675c1
ivonnelizethmoreno-cloud <ivonnelizethmoreno@gmail.com>
2025-11-06 06:26:27 -0500

    Update RunCucumberTest.java

--- src/test/java/com/grupo/RunCucumberTest.java
+++ src/test/java/com/grupo/RunCucumberTest.java
@@ -4,13 +4,16 @@ import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.ConfigurationParameter;
-import static io.cucumber.junit.platform.engine.Constants.*;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.SNIPPET_TYPE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectClasspathResource("features") // ✅ Coincide con src/test/resources/features
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.grupo.steps")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
-  value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
+        value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
 @ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase")
 public class RunCucumberTest { }
